4010a85414700e3a6ba23a8ad65d86bc7ddbb02a
Dmitry <roarlove1337@gmail.com>
2020-08-04 14:00:58 +0700

    finish refactoring subscription page

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -6,6 +6,6 @@ export class UserService {
   cardItems: ISubscriptionItem[] = [
     {id: '1', open: false, version: 'Lite', code: '6SBVGMS9TXMK2RUP', pc: 'ALFRED-PC', activated: true},
     {id: '2', open: false, version: 'Standard', code: '6SBVGMS9TXMK2RUP', pc: 'ALFRED-PC', marginTrans: true, activated: true},
-    {id: '3', open: false, version: 'Deluxe', code: '', pc: '', activated: false}
+    {id: '3', open: false, version: 'Deluxe', code: 'XUY', pc: 'ROAR-PC', activated: false}
   ]
 }
